e89176506d7c226f45e9fcb7f3ae41e92f4bd2f2
Ryan Alsobrooks <alsobrooks.ryan@gmail.com>
2024-01-30 14:29:55 -0500

    Add csv file reading

--- GroupProject1/src/csvReaderWriter.py
+++ GroupProject1/src/csvReaderWriter.py
@@ -1,8 +1,49 @@
-def readCsv(path):
-    return True
 
-def saveCsv(path, columnNames, data):
-    return True
+
+#Global csv data
+columnNames = []
+data = [[]]
+
+
+
+def readCsv(file):
+    global columnNames
+    global data
+
+    columnNames.clear()
+    data.clear()
+
+    names = file.readline()
+    for name in names.split(','):
+        columnNames.append(name.strip())
+
+    for line in file:
+        data.append(line.strip().split(','))
+
+    file.close()
+
+def saveCsv(file):
+    global columnNames
+    global data
+
+    namesOut = ""
+    for name in columnNames:
+        namesOut += name + ','
+    namesOut = namesOut.removesuffix(',')
+
+    file.write(namesOut)
+
+    for row in data:
+        rowOut = ""
+        for value in row:
+            rowOut += value + ','
+        rowOut = rowOut.removesuffix(',')
+        file.write(rowOut)
+
+    file.close()
+
+
+
 
 #
 #   Store loaded csv as...
--- GroupProject1/src/manageFrame.py
+++ GroupProject1/src/manageFrame.py
@@ -1,7 +1,22 @@
 from tkinter import *
+import tkinter
 from tkinter import ttk
+from tkinter import filedialog
+
+from csvReaderWriter import readCsv
+from csvReaderWriter import columnNames
+from csvReaderWriter import data
 
 def getManageFrame(main_frame):
     frame = ttk.Frame(master=main_frame)
     ttk.Label(master=frame, text='Manage', padding=20).pack();
+    
+    ttk.Button(master=frame, text='Open Data', command=openFileButton).pack()
+
     return frame
+
+def openFileButton():
+    file = filedialog.askopenfile()
+    readCsv(file)
+    print(columnNames)
+    print(data)
