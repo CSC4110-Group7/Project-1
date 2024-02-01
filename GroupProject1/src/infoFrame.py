2415fd6351d67089445161f1d5adeb7678d7cfa6
Ryan Alsobrooks <alsobrooks.ryan@gmail.com>
2024-02-01 17:17:06 -0500

    Remove old info frame functionality

--- GroupProject1/src/infoFrame.py
+++ GroupProject1/src/infoFrame.py
@@ -23,20 +23,3 @@ class InfoFrame:
 
     def pack(self, **kwargs):
         self.root.pack(kwargs)
-
-
-def getInfoFrame(main_frame):
-    #Read license content from file
-    current_dir = os.path.dirname(__file__)
-    filepath = os.path.join(current_dir, 'info.txt')
-    file = open(filepath, 'r')
-    content = ""
-    for line in file:
-        content += line
-    file.close()
-
-    frame = ttk.Frame(main_frame, padding=10)
-    ttk.Label(master=frame, text='Usage Info').pack()
-    ttk.Label(master=frame, text=content).pack()
-
-    return frame
